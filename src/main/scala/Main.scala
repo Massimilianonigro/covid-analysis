bf307580795d0ae1f6572f67a8b3eb24ecd7e90c
massimilianonigro <massimiliano.nigro@mail.polimi.it>
2021-03-29 15:33:42 +0200

    Broken advancement on row explosion

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,12 @@
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.catalyst.util.DateFormatter
+import org.apache.spark.sql.{ColumnName, Dataset, Row, SparkSession, functions}
+import org.apache.spark.sql.functions.{col, min}
+
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDate, LocalDateTime}
+import org.apache.spark.sql.expressions.{UserDefinedFunction, Window}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{DateType, LongType, TimestampType}
 object Main {
   def main(args: Array[String]): Unit = {
 
@@ -8,11 +15,12 @@ object Main {
       .appName("Covid-Analysis")
       .config("spark.master", "local")
       .getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
 
     import spark.implicits._
 
     var df = spark.read.csv(
-      "./resources/COVID-19-geographic-disbtribution-worldwide-2020-12-14.csv"
+      "./resources/data.csv"
     )
     df = df
       .drop("_c1") //day
@@ -47,6 +55,60 @@ object Main {
       views(country_str) =
         session.sql("SELECT * FROM `" + country_str + "`").orderBy("dateRep")
     })
-    views.values.foreach(view => view.show())
+
+    //Now for the values of each view in country we have to iterate on the days and fill the gaps
+
+    views("Afghanistan") = views("Afghanistan")
+      .withColumn("dateRep", to_date($"dateRep", "dd/MM/yyyy"))
+    val w = Window.orderBy($"dateRep")
+    val tempDf = views("Afghanistan")
+      .withColumn("diff", datediff(lead($"dateRep", 1).over(w), $"dateRep"))
+      .filter(
+        $"diff" > 1
+      ) // Pick date diff more than one day to generate our date
+      .withColumn("next_dates", fill_dates($"dateRep", $"diff"))
+      .withColumn(
+        "cases",
+        lit(compute_avg_cases($"dateRep", $"diff", views("Afghanistan")))
+      )
+      .withColumn("dateRep", explode($"next_dates"))
+      .withColumn("dateRep", $"dateRep")
+    tempDf.show()
+    views("Afghanistan") = views("Afghanistan")
+      .union(
+        tempDf
+          .select("dateRep", "cases")
+      )
+      .orderBy("dateRep")
+    views("Afghanistan").show()
+    //views.values.foreach(view => view.show())
   }
+  def fill_dates: UserDefinedFunction =
+    udf((start: String, excludedDiff: Int) => {
+      val dtFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+      val fromDt = LocalDate.parse(start, dtFormatter)
+      (1 until excludedDiff).map(day => {
+        val dt = fromDt.plusDays(day)
+        "%4d-%2d-%2d"
+          .format(dt.getYear, dt.getMonthValue, dt.getDayOfMonth)
+          .replace(" ", "0")
+      })
+    })
+
+  def compute_avg_cases(start: ColumnName, excludedDiff: ColumnName, view: Dataset[Row]): Int = {
+  {
+      var dtFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+      var fromDt = LocalDate.parse(start.toString(), dtFormatter)
+      var dt = fromDt.plusDays(excludedDiff)
+      "%4d-%2d-%2d"
+        .format(dt.getYear, dt.getMonthValue, dt.getDayOfMonth)
+        .replace(" ", "0")
+      var finalCases =
+        view.select("cases").filter("dateRep = " + dt.toString).collect()
+      var avgCases = finalCases(0)(0).asInstanceOf[Int] / excludedDiff
+      avgCases
+  }
+
+
+
 }
